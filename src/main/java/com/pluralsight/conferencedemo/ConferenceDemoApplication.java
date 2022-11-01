386a641850ef5514f7b2924a01dd82ac381ecb6e
ShameenaKoodan <shameena.koodan@gmail.com>
2022-11-01 12:09:15 -0500

    Made changes to homecontroller

--- src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.pluralsight.conferencedemo.controllers"})
+@ComponentScan(basePackages = {"com.pluralsight.conferencedemo"})
 public class ConferenceDemoApplication {
 
 	public static void main(String[] args) {
