9720f2d03fb73ac945549bd0d4c97b2969e4b63d
ShameenaKoodan <shameena.koodan@gmail.com>
2022-11-01 11:53:43 -0500

    Made mapping for main class

--- src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/pluralsight/conferencedemo/ConferenceDemoApplication.java
@@ -2,8 +2,12 @@ package com.pluralsight.conferencedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan
 public class ConferenceDemoApplication {
 
 	public static void main(String[] args) {
